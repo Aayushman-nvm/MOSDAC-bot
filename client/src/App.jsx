d04d38759d6c753fdee86c1ee74448e761035082
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-07-01 14:03:16 +0530

    Main application

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Chatbot from "./component/Chatbot";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
